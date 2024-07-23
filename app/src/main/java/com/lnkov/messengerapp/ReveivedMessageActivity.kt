e8cff43838ef656e1069550e93b7991de9a10056
Alex Lenkov <alex.lenkov123@outlook.com>
2024-07-23 20:24:39 +0300

    Initial Commit

---
+++ app/src/main/java/com/lnkov/messengerapp/ReveivedMessageActivity.kt
@@ -0,0 +1,26 @@
+package com.lnkov.messengerapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ReveivedMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reveived_message)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val textViewMessage : TextView = findViewById(R.id.textViewMessage)
+        val intent : Intent = intent
+        val message = intent.getStringExtra("message")
+        textViewMessage.text = message
+    }
+}
